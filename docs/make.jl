f83c328a33067bd31fc731859721d2b5338cd2dc
Per Rutquist <per.rutquist@gmail.com>
2026-05-10 20:23:20 +0200

    move workdir for docs to temp

--- docs/make.jl
+++ docs/make.jl
@@ -3,34 +3,29 @@ using CodeEdit
 
 DocMeta.setdocmeta!(CodeEdit, :DocTestSetup, :(using CodeEdit); recursive=true)
 
-examples_dir = joinpath(@__DIR__, "examples")
-rm(examples_dir; recursive=true, force=true)
+basedir = mktempdir()
 
-try
-    makedocs(
-        sitename = "CodeEdit.jl",
-        modules = [CodeEdit],
-        format = Documenter.HTML(
-            prettyurls = get(ENV, "CI", "false") == "true",
-        ),
-        pages = [
-            "Home" => "index.md",
-            "Getting started" => "getting-started.md",
-            "Blocks and handles" => "concepts.md",
-            "Editing code" => "editing.md",
-            "Finding errors from stacktraces" => "searching-errors.md",
-            "API reference" => "api.md",
-        ],
-        checkdocs = :none,
-        workdir = @__DIR__
+makedocs(
+    sitename = "CodeEdit.jl",
+    modules = [CodeEdit],
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", "false") == "true",
+    ),
+    pages = [
+        "Home" => "index.md",
+        "Getting started" => "getting-started.md",
+        "Blocks and handles" => "concepts.md",
+        "Editing code" => "editing.md",
+        "Finding errors from stacktraces" => "searching-errors.md",
+        "API reference" => "api.md",
+    ],
+    checkdocs = :none,
+    workdir = basedir
+)
+
+if get(ENV, "CI", "false") == "true" && haskey(ENV, "GITHUB_REPOSITORY")
+    deploydocs(
+        repo = "github.com/$(ENV["GITHUB_REPOSITORY"]).git",
+        push_preview = true,
     )
-
-    if get(ENV, "CI", "false") == "true" && haskey(ENV, "GITHUB_REPOSITORY")
-        deploydocs(
-            repo = "github.com/$(ENV["GITHUB_REPOSITORY"]).git",
-            push_preview = true,
-        )
-    end
-finally
-    rm(examples_dir; recursive=true, force=true)
 end
